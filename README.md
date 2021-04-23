45fe221714ed5e5e38721bd3469d669951c4e127
PCOS-2020LKTAAJ\Administrator <971705905@qq.com>
2021-04-23 14:53:47 +0800

    更新说明文档，增加指令文件

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # v-collapse
 vue自定义收放指令
+
+
+
+##### 使用方法：
+
+#### template：v-collapse绑定在父容器上，子元素高度设为100%。
+
+参数值：{ open：容器展开时的高度（string），close：容器收起时的高度（string），icon：按钮图标类名，style：按钮样式 }
+
+```vue
+ <div v-collapse="{open:'200px',close:'43px'}"> 
+   <el-table :data="[{ date: '2016-05-02', name: '王小虎', address: '上海市普陀区金沙江路 1518 弄' }, { date: '2016-05-04', name: '王小虎', address: '上海市普陀区金沙江路 1517 弄' }]" style="width: 100%;height:100%;"> 
+    <el-table-column prop="date" label="日期" width="180" /> 
+    <el-table-column prop="name" label="姓名" width="180" /> 
+    <el-table-column prop="address" label="地址" /> 
+   </el-table> 
+  </div>
+```
+
+#### main：
+
+```js
+import collapse from 'collapse'
+Vue.use(collapse)
+```
+
---
+++ collapse.js
@@ -0,0 +1,40 @@
+const collapse = {
+	inserted(el, binding, vnode) {
+		const { value } = binding
+		el.style.height = value.open
+		el.style.transition = 'all .4s'
+		let collapse = true
+		const collapseBtn = document.createElement('i')
+		const style = value.style ?? {
+			position: 'absolute',
+			right: '15px',
+			top: '12px',
+			zIndex: '9999',
+			transform: 'rotate(0deg)',
+			color: '#333',
+			fontSize: '14px',
+			cursor: 'pointer',
+			transition: 'all .4s'
+		}
+		Object.keys(style).forEach((key) => {
+			collapseBtn.style[key] = style[key]
+		})
+		collapseBtn.className = value.icon ?? 'el-icon-arrow-down'
+		el.appendChild(collapseBtn)
+		collapseBtn.addEventListener('click', (e) => {
+			if (collapse) {
+				e.target.style.transform = 'rotate(180deg)'
+				el.style.height = value.close
+			} else {
+				e.target.style.transform = 'rotate(0deg)'
+				el.style.height = value.open
+			}
+			collapse = !collapse
+		})
+	}
+}
+export default {
+	install(Vue) {
+		Vue.directive('collapse', collapse)
+	}
+}
